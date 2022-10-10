61fdd073d7f9a43f12a331c7b1691966f285387c
Lazariev Ivan <lazariev.ivan@lll.kpi.ua>
2022-10-10 23:35:32 +0300

    Vlasni vypravlenya

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,22 @@
 #include "calculator.h"
 
+// Adds two numbers.
 int Calculator::Add (double a, double b)
 {
+	// =)
 	return a + b + 0.5;
 }
 
+// Substracts two numbers by running Add command with negative argument.
 int Calculator::Sub (double a, double b)
 {
+	// =)
     return Add (a, -b);
 }
 
+// Multiplicates two numbers.
 int Calculator::Mul (double a, double b)
 {
+	// =)
     return a * b + 0.5;
 }
